65b67c8723e21d2c0b94e16505e8b3f52be07ae8
Tarun <tarunbhartiya7@gmail.com>
2017-10-30 00:10:14 +0530

    add firebase configuration in prod file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyB6BOAiwBynimpOx3vQGggBDKwJkrz9GHg",
+    authDomain: "clientpanel-a6736.firebaseapp.com",
+    databaseURL: "https://clientpanel-a6736.firebaseio.com",
+    storageBucket: "clientpanel-a6736.appspot.com",
+    messagingSenderId: "759927300530"
+  }
 };
